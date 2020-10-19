b7e7c6f663f2f09c5ed95819150ee1718e1b3204
Bartlomiej Wozniak <bwozniak@sii.pl>
2020-10-19 13:53:02 +0000

    Add project files.

---
+++ WebSocketChatApplication/Controllers/Chat/ChatController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketChatApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebSocketChatApplication/Extensions/WebSocketsExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace WebSocketChatApplication.Extensions
+{
+    public static class WebSocketsExtensions
+    {
+        public static IApplicationBuilder UseWebSocket(this IApplicationBuilder app)
+        {
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+
+            app.UseWebSockets(webSocketOptions);
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await Echo(webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+
+            });
+
+            return app;
+        }
+
+        private async static Task Echo(WebSocket webSocket)
+        {
+            var messageBuffer = Encoding.ASCII.GetBytes("hej");
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(new ArraySegment<byte>(messageBuffer, 0, messageBuffer.Length), WebSocketMessageType.Text, result.EndOfMessage, CancellationToken.None);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
